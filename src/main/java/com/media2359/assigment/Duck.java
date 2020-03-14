953f24d76c4a534f96a5a134fb3e869bf7da83c5
votanphuc <votanphuc@gmail.com>
2020-03-14 19:52:52 +0700

    + Add Duck and Chicken

---
+++ src/main/java/com/media2359/assigment/Duck.java
@@ -0,0 +1,13 @@
+package main.java.com.media2359.assigment;
+
+public class Duck extends Bird {
+    @Override
+    public void sing() {
+        System.out.println("Quack, quack");
+    }
+
+    public void swim() {
+        System.out.println("I am swimming");
+
+    }
+}
